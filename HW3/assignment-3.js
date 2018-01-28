1cda65f5f73b1a5cb498c9e72404952f1ab57526
Steve Finch <git@stevefinch.com>
2018-01-28 11:49:27 -0800

    added edit boxes (not working yet)

--- HW3/assignment-3.js
+++ HW3/assignment-3.js
@@ -13,15 +13,67 @@ window.onload=function(){
         nav.style.background = '#FF4500'; 
         }
     )
+
+    var par = document.getElementById('par-0');
+    var txt = document.getElementById('text-0');
+    par.addEventListener('dblclick', function() {
+        var place_holder = par.value;
+        txt.value = place_holder;
+        _switch0();
+      });
+
+    var par = document.getElementById('par-1');
+    var txt = document.getElementById('text-1');
+    par.addEventListener('dblclick', function() {
+        var place_holder = par.value;
+        txt.value = place_holder;
+        _switch0();
+    });
+        
+      document.getElementById('par-1').addEventListener('dblclick', function() {
+        var place_holder = document.getElementById('par-1').value;
+        console.log(place_holder);
+        document.getElementById('text-1').value = place_holder;
+      
+        // console.log(pclick());
+        // console.log(place_holder);
+        _switch0();
+      });
+      
+      document.getElementById('button-0').addEventListener('click', function() {
+          var place_holder = document.getElementById('text-0').innerHTML;
+          document.getElementById('par-0').innerHTML = place_holder;
+          _switch1();
+      });
+      
+      document.getElementById('button-1').addEventListener('click', function() {
+          var place_holder = document.getElementById('text-1').innerHTML;
+          document.getElementById('par-1').innerHTML = place_holder;
+          _switch1();
+      });
 }
 
-var el = document.getElementsByClassName("big_picture");
-if (el[0]) {
-    el[0].addEventListener('click', function() {
-        alert('Its a test, ya dick!'); 
-    });
-} 
+function pclick(){
+	if (document.getElementById('par-0')) {
+		return 0;
+	}
 
+	else {
+		return 1;
+	}
+}
+
+function _switch0(){
+	document.querySelector('p').style.display = 'none';
+	document.querySelector('textarea').style.display = 'inline';
+	document.querySelector('button').style.display = 'inline'
+}
+
+function _switch1(){
+	document.querySelector('p').style.display = 'inline';
+	document.querySelector('textarea').style.display = 'none';
+	document.querySelector('button').style.display = 'none'
+}
 
 function Xclicked(X){  
  X.parentElement.style.display = "none";
